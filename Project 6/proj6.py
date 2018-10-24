7d09aeca65e64782267f282531c7a94b3f1ac19e
Qwertie64982 <maxwell.a.sherman@live.com>
2018-10-23 22:21:59 -0700

    Tokenize corpus for P6

--- Project 6/proj6.py
+++ Project 6/proj6.py
@@ -8,8 +8,18 @@ Due: 2018-10-26 18:00 PDT
 '''
 
 from nltk.corpus import brown
+import re
 
 def main():
-    print "a"
+    tmp = brown.sents(categories = "editorial")
+    new = [[word.encode("ascii") for word in sentence] for sentence in tmp]
+    
+    for i, sentence in enumerate(new):
+        for j, word in enumerate(sentence):
+            new[i][j] = re.sub("[.,?!\"]+", "", word.lower()) # make lowercase and remove special chars except apostrophe
+        new[i] = filter(None, new[i]) # remove empty strings
+    
+    print new[0]
+    print new[1]
 
 main()
