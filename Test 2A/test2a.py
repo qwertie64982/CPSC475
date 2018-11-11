cb48906ee475dde841d603769339e62f7a584b3c
Qwertie64982 <maxwell.a.sherman@live.com>
2018-11-10 20:05:21 -0800

    Sad start on test 2A

---
+++ Test 2A/test2a.py
@@ -0,0 +1,21 @@
+'''
+Team Member #1: Maxwell Sherman
+Team Member #2: N/A
+Zagmail address for team member 1: msherman3@zagmail.gonzaga.edu
+Test 2A: This program implements the forward algorithm on the Eisner problem
+Usage: python test2a.py 3 1 3 (3 1 3 is an example of an ice cream eating sequence)
+Due: 2018-11-16 18:00 PDT
+'''
+
+def main():
+    # TODO: read these from files later
+    aMatrix = [[0.0, 0.5, 0.5, 0.0], [0.0, 0.7, 0.2, 0.1], [0.0, 0.2, 0.7, 0.1], [0.0, 0.0, 0.0, 0.0]]
+    bMatrix = [[0.0, 0.0, 0.0], [0.6, 0.3, 0.1], [0.1, 0.3, 0.6], [0.0, 0.0, 0.0]]
+    
+    # TODO: get these from command line args
+    # check for anything other than 1, 2, 3 in numbers, if so return 0
+    sequence = [3, 1, 3]
+    
+    # I don't know how to represent the trellis
+
+main()
