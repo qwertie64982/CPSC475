8b84c2d8ea207fb522a819afe65fd04f5ea692c1
Qwertie64982 <maxwell.a.sherman@live.com>
2018-11-14 17:19:34 -0800

    Marginal progress

--- Test 2A/test2a.py
+++ Test 2A/test2a.py
@@ -9,24 +9,34 @@ Due: 2018-11-16 18:00 PDT
 
 def main():
     # TODO: read these from files later
-    observations = [[0.0, 0.5, 0.5, 0.0], [0.0, 0.7, 0.2, 0.1], [0.0, 0.2, 0.7, 0.1], [0.0, 0.0, 0.0, 0.0]]
-    states = [[0.0, 0.0, 0.0], [0.6, 0.3, 0.1], [0.1, 0.3, 0.6], [0.0, 0.0, 0.0]]
+    states = [[0.0, 0.5, 0.5, 0.0], [0.0, 0.7, 0.2, 0.1], [0.0, 0.2, 0.7, 0.1], [0.0, 0.0, 0.0, 0.0]] # A-matrix
+    observations = [[0.0, 0.0, 0.0], [0.6, 0.3, 0.1], [0.1, 0.3, 0.6], [0.0, 0.0, 0.0]] # B-matrix
     
     # TODO: get these from command line args
     # check for anything other than 1, 2, 3 in numbers, if so return 0
     sequence = [3, 1, 3]
     
-    forwardProb = forward(states, sequence)
+    forwardProb = forward(states, observations, sequence)
 
-def forward(states, sequence):
+def forward(states, observations, sequence):
     numStates = len(states)
-    numObs = len(sequence)
-    forward = [[0 for j in range(numObs + 2)] for i in range(numStates + 2)]
+    lenSequence = len(sequence)
+    forward = [[0 for j in range(lenSequence + 2)] for i in range(numStates + 2)]
     forward[0][0] = 1.0
-    for t in range(numObs):
+    for t in range(lenSequence):
         for s in range(numStates):
-            for # ?????
-                # ?????
-    return # sum forward[last][all]
+            for transitionIndex in range(len(states[s])):
+                x = forward[s][t]
+                y = states[s][transitionIndex]
+                z = observations[transitionIndex][sequence[t]-1]
+                # print x,
+                # print y,
+                # print z,
+                print x * y * z,
+                print "into [" + str(transitionIndex) + "][" + str(t+1) + "]" # TODO: Why does it overwrite?
+                
+                forward[transitionIndex][t+1] = x * y * z
+    print str(forward)
+    return 0
 
 main()
