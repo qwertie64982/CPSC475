f7956977ee70835fefd8bb140dc2e3d78fd5ba71
Qwertie64982 <maxwell.a.sherman@live.com>
2018-11-27 21:21:30 -0800

    Cleaned up code, still a small algorithm issue

--- Test 2B/test2b.py
+++ Test 2B/test2b.py
@@ -2,9 +2,9 @@
 Team Member #1: Maxwell Sherman
 Team Member #2: N/A
 Zagmail address for team member 1: msherman3@zagmail.gonzaga.edu
-Test 2A: This program implements the forward algorithm on the Eisner problem
-Usage: python test2a.py 3 1 3 (3 1 3 is an example of an ice cream eating sequence)
-Due: 2018-11-19 18:00 PDT
+Test 2A: This program implements the Viterbi algorithm on the Eisner problem
+Usage: python test2b.py matA.csv matB.csv 3 1 3 (3 1 3 is an example of an ice cream eating sequence)
+Due: 2018-11-30 18:00 PDT
 '''
 
 import csv
@@ -30,18 +30,18 @@ def main():
         try:
             sequence[i] = int(sequence[i])
             if sequence[i] < 1 or sequence[i] > 3:
-                print("This is an impossible scenario") # 0% probability of Eisner eating < 1 or > 3 ice cream cones in one day TODO: modify this
+                print("This is an impossible scenario") # no probability of Eisner eating < 1 or > 3 ice cream cones in one day
         except ValueError:
             print("ERROR: Arguments must all be numbers")
             sys.exit(1)
     
-    # construct forward trellis
-    forwardMatrix, backPointers = forward(states, observations, sequence)
+    # construct Viterbi trellis
+    viterbiMatrix, backPointers = viterbi(states, observations, sequence)
     
-    printResults(forwardMatrix, backPointers, len(sequence))
+    printResults(viterbiMatrix, backPointers, len(sequence))
     
-    for line in forwardMatrix:
-        print str(line)
+    # for line in viterbiMatrix:
+    #     print str(line)
 
 # read a CSV file into a list
 def readCSV(filename):
@@ -50,61 +50,49 @@ def readCSV(filename):
     result = numpy.array(inList).astype("float")
     return result
 
-# calculate forward trellis
+# calculate Viterbi trellis
 # the trellis is represented as a 2D array, where 3, 1, 3 is the example sequence:
 #        3  1  3
 # start [ ][ ][ ]
 #  cold [ ][ ][ ]
 #   hot [ ][ ][ ]
 #   end [ ][ ][ ]
-def forward(states, observations, sequence):
+def viterbi(states, observations, sequence):
     # make empty matrix
-    forward = [[0 for observation in sequence] for state in range(len(states))]
+    viterbiMatrix = [[0 for observation in sequence] for state in range(len(states))]
     backPointers = [[-1 for observation in sequence] for state in range(len(states))]
     
     # fill matrix
-    forward = initialize(forward, states, observations, sequence) # fill first column
-    forward = fill(forward, backPointers, states, observations, sequence) # fill following columns
+    viterbiMatrix = initialize(viterbiMatrix, states, observations, sequence) # fill first column
+    viterbiMatrix = fill(viterbiMatrix, backPointers, states, observations, sequence) # fill following columns
     
-    return forward, backPointers
+    return viterbiMatrix, backPointers
 
-# fill the first column of the forward trellis
-def initialize(forward, states, observations, sequence):
-    for stateIndex in range(len(forward)):
+# fill the first column of the Viterbi trellis
+def initialize(viterbiMatrix, states, observations, sequence):
+    for stateIndex in range(len(viterbiMatrix)):
         newProb = states[0][stateIndex] * observations[stateIndex][sequence[sequence[0]-1]-1]
-        if (forward[stateIndex][0] == 0 or newProb > forward[stateIndex][sequence[0]-1]):
-            forward[stateIndex][0] = newProb
-    return forward
+        if (viterbiMatrix[stateIndex][0] == 0 or newProb > viterbiMatrix[stateIndex][sequence[0]-1]):
+            viterbiMatrix[stateIndex][0] = newProb
+    return viterbiMatrix
 
-# fill the rest of the columns in the forward trellis
-def fill(forward, backPointers, states, observations, sequence):
+# fill the rest of the columns in the Viterbi trellis
+def fill(viterbiMatrix, backPointers, states, observations, sequence):
     for seqIndex in range(1, len(sequence)): # -1 because we already did the first column
-        for stateIndex in range(len(forward)):
-            for stateIndex2 in range(len(forward)): # sum the probabilities of getting to this state from every previous possible state
-                newProb = forward[stateIndex2][seqIndex-1] * states[stateIndex2][stateIndex] * observations[stateIndex][sequence[seqIndex]-1]
-                if (forward[stateIndex][seqIndex] == 0 or newProb > forward[stateIndex][seqIndex]):
-                    forward[stateIndex][seqIndex] = newProb
+        for stateIndex in range(len(viterbiMatrix)):
+            for stateIndex2 in range(len(viterbiMatrix)): # find the largest of the probabilities of getting to this state from every previous possible state
+                newProb = viterbiMatrix[stateIndex2][seqIndex-1] * states[stateIndex2][stateIndex] * observations[stateIndex][sequence[seqIndex]-1]
+                if (viterbiMatrix[stateIndex][seqIndex] == 0 or newProb > viterbiMatrix[stateIndex][seqIndex]):
+                    viterbiMatrix[stateIndex][seqIndex] = newProb
                     backPointers[stateIndex][seqIndex] = stateIndex2
-    for line in backPointers:
-        print line
-    print
     
-    return forward
+    return viterbiMatrix
 
-# sum the last column of the trellis
-# technically, every item in the last column points to the end state,
-# so summing them all is really calculating the value of the end state
-def sumEnds(forward):
-    endingProb = 0.0
-    for i in range(len(forward)):
-        endingProb += forward[i][-1]
-    return endingProb
-
-def printResults(forward, backPointers, maxIndex):
+def printResults(viterbiMatrix, backPointers, maxIndex):
     statesList = [-1]
     
     runningMax = 0.0
-    for i, row in enumerate(forward):
+    for i, row in enumerate(viterbiMatrix):
         if row[maxIndex-1] > runningMax:
             runningMax = row[maxIndex-1]
             statesList[0] = i
@@ -112,7 +100,21 @@ def printResults(forward, backPointers, maxIndex):
     for j in range(maxIndex-1, 0, -1):
         statesList.append(backPointers[statesList[-1]][j])
     
-    # statesList now contains numbers 0-3 representing the most probable states (start, cold, hot, end)
-    # TODO: turn these into human-readable strings
+    # print statesList
+    
+    # start and end are included here, but are unused due to the Viterbi matrix implementation
+    print("State sequence:"),
+    for state in statesList:
+        if state == 0:
+            print("start"),
+        elif state == 1:
+            print("cold"),
+        elif state == 2:
+            print("hot"),
+        elif state == 3:
+            print("end"),
+        else:
+            print("ERROR: Invalid state")
+            sys.exit(1)
 
 main()
